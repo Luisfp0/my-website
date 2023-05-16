6d213e46dc639413bc019a747ee231058a2c2aaa
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-16 14:04:20 -0300

    Formatado as informações de contato

---
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -0,0 +1,32 @@
+import './ContactInfo.css'
+import logoInsta from '../../assets/images/icon-instagram.svg'
+import logoTwitter from '../../assets/images/icon-twitter.svg'
+import logoDribbble from '../../assets/images/icon-dribbble.svg'
+import logoWhatsapp from  '../../assets/images/icon-whatsapp.svg'
+
+function ContactInfo() {
+  return (
+    <div className='container-contact-info'>
+        <h2 className='title-contact' >Contato</h2>
+        <p>luisfernandodepaulosousa0020@gmail.com</p>
+        <p>+55 62 9 9322 2660</p>
+        <div className="social-links">
+          <a href="https://www.instagram.com/n_andooo/" className="social">
+            <img src={logoInsta} alt="Instagram" />
+          </a>
+          <a href="https://twitter.com/Louisfp0" className="social">
+            <img src={logoTwitter} alt="Twitter" />
+          </a>
+          <a href="#" className="social">
+            <img src={logoDribbble} alt="Dribbble" />
+          </a>
+          <a href="https://wa.me/5562993222660" className="social">
+            <img src={logoWhatsapp} alt="Whatsapp" />
+          </a>
+      </div>
+      <div className="corner"></div>
+    </div>
+  )
+}
+
+export default ContactInfo
--- src/components/ContentPage/ContentPage.jsx
+++ src/components/ContentPage/ContentPage.jsx
@@ -2,6 +2,7 @@ import './ContentPage.css'
 import Header from '../Header/Header'
 import Presentation from '../Presentation/Presentation'
 import Contact from '../Contact/Contact'
+import ContactInfo from '../ContactInfo/ContactInfo'
 
 function ContentPage() {
   return (
@@ -10,6 +11,7 @@ function ContentPage() {
       <section className="content-page">
         <Presentation/>
         <Contact></Contact>
+        <ContactInfo></ContactInfo>
       </section>
     </div>
   )
