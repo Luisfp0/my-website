df9ee35b545456757bd765e9f24168110c5030d0
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-22 13:27:56 -0300

    Feito a barra lateral, posicionado o projects e  o texto dentro da image

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,28 +1,42 @@
 import './Projects.css'
-import imageOne from '../../assets/images/image-1.jpg'
-import imageTwo from '../../assets/images/image-2.jpg'
+import imageOne from '../../assets/images/to-do-list-project.png'
+import imageTwo from '../../assets/images/matching-game-project.png'
 import imageThree from '../../assets/images/image-3.jpg'
 import imageFour from '../../assets/images/image-4.jpg'
 import imageFive from '../../assets/images/image-5.jpg'
 
 function Projects(props) {
-  const imageProjects = [
+  const imageProjects = [ 
     {id: 1,
-    src: imageOne},
+    src: imageOne,
+    info:'Uma lista de tarefas feita utilizando react, sendo possível adicionar, concluir e remover tarefas. Tendo integração com o local storage, então sempre que voltar as tarefas estarão salvas.',
+    title: 'To-do List'},
     {id: 2,
-    src: imageTwo},
+    src: imageTwo,
+    info:'info projeto 2 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
+    title: 'Title project2'},
     {id: 3,
-    src: imageThree},
+    src: imageOne,
+    info:'info projeto 3 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
+    title: 'Title project3'},
     {id: 4,
-    src: imageFour},
+    src: imageOne,
+    info:'info projeto 4 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
+    title: 'Title project4'},
     {id: 5,
-    src: imageFive}]
+    src: imageOne,
+    info:'info projeto 5 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
+    title: 'Title project5'}]
   return (
     <div className='slider-container'>
       <div className="slider">
       {imageProjects.map((project) => {
         if(project.id === props.currentProject) {
-        return <img src={project.src} className='active'/>
+        return <div className='container-project'>
+          <h1 className='title-project'>{project.title}</h1>
+          <p className='info'>{project.info}</p>
+          <img src={project.src} className='active'/>
+        </div>
         }
       })}
       </div>
