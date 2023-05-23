7c722dc47e758197e1f703e1e015813f964ed6c4
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-23 11:39:07 -0300

    formatado a caixa de explicação e adicionado o link de cada projeto

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,22 +8,25 @@ function Projects(props) {
     {id: 1,
     src: imageOne,
     info:'Uma lista de tarefas feita utilizando react, sendo possível adicionar, concluir e remover tarefas. Tendo integração com o local storage, então sempre que voltar as tarefas estarão salvas.',
-    title: 'To-do List'},
+    title: 'To-do List',
+    link: 'https://to-do-list-mu-pied.vercel.app'},
     {id: 2,
     src: imageTwo,
     info:'Um jogo da memória feito em html, css e javascript, mas que também tem uma versão feita utilizando react.',
-    title: 'Matching Game'},
+    title: 'Matching Game',
+    link: 'https://curso-de-js-para-react.vercel.app'},
     {id: 3,
     src: imageThree,
-    info:'info projeto 3 - Lorem iasdasdpsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
-    title: 'Title project3'},
+    info:'Um jogo do Mario feito em html, css e javascript, onde o objetivo é pular antes do cano atingi-lo.',
+    title: 'Mario Game',
+    link: 'https://mario-game-html-css-js.vercel.app'},
     {id: 4,
     src: imageOne,
-    info:'info projeto 4 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
+    info:'info projeto 4 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum.',
     title: 'Title project4'},
     {id: 5,
     src: imageOne,
-    info:'info projeto 5 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum repudiandae atque obcaecati corrupti non.',
+    info:'info projeto 5 - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt recusandae porro fuga hic impedit aperiam ratione quidem praesentium doloremque nisi, sint officiis quibusdam provident eum.',
     title: 'Title project5'}]
   return (
     <div className='slider-container'>
@@ -34,6 +37,9 @@ function Projects(props) {
           <div className='container-info'>
             <h1 className='title-project'>{project.title}</h1>
             <p className='info'>{project.info}</p>
+            <button className='btn-more'>
+              <a href={project.link}>More</a>
+            </button>
           </div>
           <img src={project.src} className='active'/>
         </div>
