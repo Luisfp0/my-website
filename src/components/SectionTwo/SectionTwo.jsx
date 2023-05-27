6141e2b71152c2f8b145c4aa25df3c2887ee0c30
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-26 21:28:38 -0300

    Feito a formatação do objeto about

--- src/components/SectionTwo/SectionTwo.jsx
+++ src/components/SectionTwo/SectionTwo.jsx
@@ -2,27 +2,38 @@ import './SectionTwo.css'
 
 function SectionTwo() {
   const arrayMe = [{
-    human: `let human? = [{`,
-    name: `name: Luís Fernando,`,
+    human: `let human? = {`,
+    name: `name: 'Luís Fernando',`,
     age:`age: 21,`,
     height: `188,`,
-    coder: `class: coder,`,
-    subclass: `subclass: jedi`,
-    skills: `skills: html, css, js, react`,
-    fechamento: `}];`
+    coder: `class: 'coder',`,
+    subclass: `subclass: 'jedi',`,
+    skills: `skills: 'html, css, js, react'`,
+    fechamento: `};`
   }]
   return (
     <section className="section-two">
       <div className='wrap-about'>
         <div className='container-object'>
+          <div className='grade-container'>
+            <span className='grade'>1</span>
+            <span className='grade'>2</span>
+            <span className='grade'>3</span>
+            <span className='grade'>4</span>
+            <span className='grade'>5</span>
+            <span className='grade'>6</span>
+            <span className='grade'>7</span>
+          </div>
           {arrayMe.map((info) =>{return <div>
-            <span className='span-object'>{info.human}</span>
-            <span className='span-object'>{info.name}</span>
-            <span className='span-object'>{info.age}</span>
-            <span className='span-object'>{info.coder}</span>
-            <span className='span-object'>{info.subclass}</span>
-            <span className='span-object'>{info.skills}</span>
-            <span className='span-object'>{info.fechamento}</span>
+            <div> 
+              <span className='span-object'>{info.human}</span>
+              <span className='span-object'>{info.name}</span>
+              <span className='span-object'>{info.age}</span>
+              <span className='span-object'>{info.coder}</span>
+              <span className='span-object'>{info.subclass}</span>
+              <span className='span-object'>{info.skills}</span>
+              <span className='span-object'>{info.fechamento}</span>
+            </div>
           </div>})}
         </div>
         <div className='container-about'>
