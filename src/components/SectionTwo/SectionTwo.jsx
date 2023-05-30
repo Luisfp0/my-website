12752a45dce03c0ab666b95a4429c69a7bdb5a5d
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-30 11:51:32 -0300

    Adicionado fiveicon a página

--- src/components/SectionTwo/SectionTwo.jsx
+++ src/components/SectionTwo/SectionTwo.jsx
@@ -41,7 +41,7 @@ function SectionTwo(props) {
         <div className='container-about'>
           <h1>About me</h1>
           <p> Hi ! I am a software enginner based on Brazil.</p>
-          <p> I started programming a little over 6 months ago and never imagined I would fall in love so fast with an area, which with time is proving to be more and more amazing.</p>
+          <p> I started programming a little over 6 months ago and never imagined I would fall in love so fast with something, which with time is proving to be more and more amazing.</p>
           <p> Currently I'm studying (ADS) and focused on Front-end development, but I don't plan to stop here, my goal is to expand my knowledge as much as possible.</p>
         </div>
       </div>
